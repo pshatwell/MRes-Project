922108a53a5dce0d8ea0c291ff6020b12ac07d4d
Peter Shatwell <peter.shatwell12@imperial.ac.uk>
2017-02-13 13:10:34 +0000

    Played around with scalings and orders of magnitude. Parcel trajectories currently look good, but maybe not physical. Need to understand how to translate the solutions to physical quantities.

--- RossbyInfo.py
+++ RossbyInfo.py
@@ -5,12 +5,15 @@ import numpy as np
 
 L = 1e5 #(m) typical length scale
 f0 = 1e-4 #(s^-1) Coriolis parameter
-T = 1./f0 #(s) typical time scale
+#T = 1./f0 #(s) typical time scale
+T = 100
 k = 2e-5 #(m^-1) horizontal wavenumber
 a = 3e4 #(m) deformation radius
 psi0 = 3e3 #(m^2*s^-1) wave amplitude
 beta = 3e-11 #(m^-1*s^-1) latitudinal variation of Coriolis parameter
 c = -beta*a**2 #(m*s^-1) phase speed of wave
+A_und = (psi0*np.pi)/((L**2)*f0) #non-dimensional amplitude of uprime
+A_vnd = (psi0*k)/(L*f0) #non-dimensional amplitude of vprime
 
 #Define streamfunction and its derivatives as velocity perturbations
 #Make x,y and t all non-dimensional
@@ -18,7 +21,15 @@ def psiprime(x,y,t):
     return psi0*np.cos(k*L*x - c*T*t)*np.sin(np.pi*y)
 
 def uprime(x,y,t): #zonal velocity
-    return -((psi0*np.pi)/((L**2)*f0))*np.cos(k*L*x - c*T*t)*np.cos(np.pi*y)
+    #return -A_und*np.cos(k*L*x - c*T*t)*np.cos(np.pi*y)
+    return np.cos(k*L*x - c*T*t)*np.cos(np.pi*y)
 
 def vprime(x,y,t): #meridional velocity
-    return -((psi0*k)/(L*f0))*np.sin(k*L*x - c*T*t)*np.sin(np.pi*y)
+    #return -A_vnd*np.sin(k*L*x - c*T*t)*np.sin(np.pi*y)
+    return np.sin(k*L*x - c*T*t)*np.sin(np.pi*y)
+
+def testuprime(x,y,t):
+    return -np.cos(2*x - t)*np.cos(2*y)
+
+def testvprime(x,y,t):
+    return -np.sin(2*x - t)*np.sin(2*y)
--- SimpleRossbyNew.py
+++ SimpleRossbyNew.py
@@ -9,13 +9,25 @@ import matplotlib.pyplot as plt
 #PRINT OUT PHYSICAL QUANTITIES FOR THE WAVE AND DIMENSIONAL SCALINGS
 
 print 'phase speed, c (ms^(-1)) is:', c
+
 print 'amplitude of uprime (in ms^(-1)), psi0*pi/L, is:', (psi0*np.pi)/L #amplitude of uprime
 print 'amplitude of vprime (in ms^(-1)), psi0*k, is:', psi0*k #amplitude of vprime
 
-print 'nondimensional amplitude of uprime_nd, (psi0*pi)/(L**2*f0), is:', (psi0*np.pi)/((L**2)*f0) #non-dimensional amplitude of uprime
-print 'nondimensional amplitude of vprime_nd, (psi0*k)/(L*f0), is:', (psi0*k)/(L*f0) #non-dimensional amplitude of vprime
-
 print 'Scaling factor to dimensionalise velocity equations (in ms^(-1)), L*f0 is:', L*f0
+
+print 'uprime_nd is: -((psi0*np.pi)/((L**2)*f0))*np.cos(k*L*x - c*T*t)*np.cos(np.pi*y)'
+print 'vprime_nd is: -((psi0*k)/(L*f0))*np.sin(k*L*x - c*T*t)*np.sin(np.pi*y)'
+
+print 'amplitude of uprime_nd, (psi0*pi)/(L**2*f0), is:', A_und
+print 'amplitude of vprime_nd, (psi0*k)/(L*f0), is:', A_vnd
+
+print 'k is:', k
+print 'L is:', L
+print 'k*L is:', k*L
+
+print 'c is:', c
+print 'T is:', T
+print 'c*T is:', c*T
 
 ###############################################################################################
 
@@ -33,14 +45,15 @@ def main():
     #CREATE ARRAY FOR PSIPRIME VALUES
     psiprime_nd = np.zeros((ylength,xlength,tlength)) #Note x and y have switched so plotted output is oriented correctly
 
+    #Indices in array go [y,x,t]
     for i in range(0, ylength, 1):
         for j in range(0, xlength, 1):
-            for k in range(0, tlength, 1):
+            for m in range(0, tlength, 1):
                 #print 'x is: ', xvalues[j]
                 #print 'y is: ', yvalues[i]
                 #print 't is: ', tvalues[k]
 
-                psiprime_nd[i,j,k] = psiprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
+                psiprime_nd[i,j,m] = psiprime(x=xvalues[j], y=yvalues[i], t=tvalues[m])
                 #print 'uprime_nd is (from function):', uprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
                 #print 'uprime_nd is (from array):', uprime_nd[i,j,k]
 
@@ -55,12 +68,12 @@ def main():
 
     for i in range(0, ylength, 1):
         for j in range(0, xlength, 1):
-            for k in range(0, tlength, 1):
+            for m in range(0, tlength, 1):
                 #print 'x is: ', xvalues[j]
                 #print 'y is: ', yvalues[i]
                 #print 't is: ', tvalues[k]
 
-                uprime_nd[i,j,k] = uprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
+                uprime_nd[i,j,m] = uprime(x=xvalues[j], y=yvalues[i], t=tvalues[m])
                 #print 'uprime_nd is (from function):', uprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
                 #print 'uprime_nd is (from array):', uprime_nd[i,j,k]
 
@@ -75,12 +88,12 @@ def main():
 
     for i in range(0, ylength, 1): #Note x and y have switched so plotted output is oriented correctly
         for j in range(0, xlength, 1):
-            for k in range(0, tlength, 1):
+            for m in range(0, tlength, 1):
                 #print 'x is: ', xvalues[j]
                 #print 'y is: ', yvalues[i]
                 #print 't is: ', tvalues[k]
 
-                vprime_nd[i,j,k] = vprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
+                vprime_nd[i,j,m] = vprime(x=xvalues[j], y=yvalues[i], t=tvalues[m])
                 #print 'vprime_nd is (from function):', vprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
                 #print 'vprime_nd is (from array):', vprime_nd[i,j,k]
 
@@ -88,27 +101,35 @@ def main():
     print 'shape of vprime_nd is:', vprime_nd.shape
     #print 'vprime_nd x values are:', vprime_nd[:,0,0]
 
-    print 'vprime_nd at y = 0 is:', vprime_nd[2,0,2]
-    print 'vprime_nd at y = L is:', vprime_nd[2,-1,2]
+    print 'vprime_nd at y = 0 is:', vprime_nd[0,2,2]
+    print 'vprime_nd at y = L is:', vprime_nd[-1,2,2]
 
 ###############################################################################################
 
     #DEFINE ODE TO INTEGRATE FOR PARCEL TRAJECTORIES
-    #???WHY DON'T THE PARCELS MOVE VERY FAR AT ALL??
 
+    #SOMETHING VERY WRONG WITH ORDERS OF MAGNITUDE SOMEWHERE. AMPLITUDES ARE ~1e-3, cT~1e2,
+    #WHICH MEANS 1e-5 SCALING OF TRAJECTORIES. VERY SMALL LENGTHS, VERY FAST OSCILLATIONS.
+    #WHAT PARAMETERS ARE INCORRECT HERE?
+
+    #TRY SOLVING THIS ANALYTICALLY BY HAND AND PLOTTING THAT
+
+    #CURRENTLY SOLVING UPRIME AND VPRIME WITH AMPLITUDES = 1. T = 100.
     def velocitynd(s,t):
         x, y = s
         dsdt = [uprime(x=x,y=y,t=t), vprime(x=x,y=y,t=t)]
+        #dsdt = [testuprime(x=x,y=y,t=t), testvprime(x=x,y=y,t=t)]
         return dsdt
 
-    t=np.linspace(0,2,200)
-    s0_a = [0.3, 0.25]
-    s0_b = [0.3, 0.25001]
-    s0_c = [0.5,0.5]
+    t=np.linspace(0,10,200)
 
-    sol_a = odeint(velocitynd, s0_a, t)
-    sol_b = odeint(velocitynd, s0_b, t)
-    sol_c = odeint(velocitynd, s0_c, t)
+    s0_a = [0.5, 0.5]
+    s0_b = [0.3, 0.3]
+    #s0_c = [0.5, 0.5]
+
+    sol_a = odeint(velocitynd, s0_a, t, mxstep=1000000)
+    sol_b = odeint(velocitynd, s0_b, t, mxstep=1000000)
+    #sol_c = odeint(velocitynd, s0_c, t)
 
 ###############################################################################################
 
@@ -135,7 +156,7 @@ def main():
     ax4 = fig2.add_subplot(111)
     ax4.plot(sol_a[:,0], sol_a[:,1], label='trajectory a')
     ax4.plot(sol_b[:,0], sol_b[:,1], label='trajectory b')
-    ax4.plot(sol_c[:,0], sol_c[:,1], label='trajectory c')
+    #ax4.plot(sol_c[:,0], sol_c[:,1], label='trajectory c')
     plt.legend(loc='best')
 
     plt.show()
