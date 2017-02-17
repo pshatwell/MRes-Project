ead700e05beaa3d8dff5b06aabdcae564d1f24d5
Peter Shatwell <peter.shatwell12@imperial.ac.uk>
2017-02-17 09:46:35 +0000

    FIXED IT! I think. The bloody angular frequency was not multiplied by k before... And I have now transformed trajectories to Rossby wave frame.

--- RossbyInfo.py
+++ RossbyInfo.py
@@ -3,15 +3,19 @@ import numpy as np
 #PARAMETER VALUES AND FUNCTIONS USED FOR ROSSBY WAVE SCRIPTS
 #SET DIMENSIONAL SCALES FOR VARIABLES
 
-L = 1e5 #(m) typical length scale
+L = 1e6 #(m) typical length scale
 f0 = 1e-4 #(s^-1) Coriolis parameter
 T = 1./f0 #(s) typical time scale
-#T = 1000
-k = 2e-5 #(m^-1) horizontal wavenumber
+#T = 1e5
+k = 1e-5 #(m^-1) horizontal wavenumber
 a = 3e4 #(m) deformation radius
-psi0 = 3e3 #(m^2*s^-1) wave amplitude
+psi0 = 3e4 #(m^2*s^-1) wave amplitude
 beta = 3e-11 #(m^-1*s^-1) latitudinal variation of Coriolis parameter
 c = -beta*(a**2) #(m*s^-1) phase speed of wave
+wavelength = (2*np.pi)/k #horizontal wavelength of wave
+
+A_u = (psi0*np.pi)/L #amplitude of uprime
+A_v = psi0*k #amplitude of vprime
 
 A_und = (psi0*np.pi)/((L**2)*f0) #non-dimensional amplitude of uprime
 A_vnd = (psi0*k)/(L*f0) #non-dimensional amplitude of vprime
@@ -19,15 +23,15 @@ A_vnd = (psi0*k)/(L*f0) #non-dimensional amplitude of vprime
 #Define streamfunction and its derivatives as velocity perturbations
 #Make x,y and t all non-dimensional
 def psiprime(x,y,t):
-    return psi0*np.cos(k*L*x - c*T*t)*np.sin(np.pi*y)
+    return psi0*np.cos(k*L*x - k*c*T*t)*np.sin(np.pi*y)
 
 def uprime(x,y,t): #zonal velocity, equal to -psiprime_y
-    #return -A_und*np.cos(k*L*x - c*T*t)*np.cos(np.pi*y)
-    return -np.cos(k*L*x - c*T*t)*np.cos(np.pi*y)
+    return -A_u*np.cos(k*L*x - k*c*T*t)*np.cos(np.pi*y)
+    #return -np.cos(k*L*x - k*c*T*t)*np.cos(np.pi*y)
 
 def vprime(x,y,t): #meridional velocity, equal to psiprime_x
-    #return -A_vnd*np.sin(k*L*x - c*T*t)*np.sin(np.pi*y)
-    return -np.sin(k*L*x - c*T*t)*np.sin(np.pi*y)
+    return -A_v*np.sin(k*L*x - k*c*T*t)*np.sin(np.pi*y)
+    #return -np.sin(k*L*x - k*c*T*t)*np.sin(np.pi*y)
 
 def testuprime(x,y,t):
     return -np.cos(2*x - t)*np.cos(2*y)
--- SimpleRossbyNew.py
+++ SimpleRossbyNew.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 #PRINT OUT PHYSICAL QUANTITIES FOR THE WAVE AND DIMENSIONAL SCALINGS
 
 print 'phase speed, c (ms^(-1)) is:', c
+print 'horizontal wavelength (m) is:', wavelength
 
 print 'amplitude of uprime (in ms^(-1)), psi0*pi/L, is:', (psi0*np.pi)/L #amplitude of uprime
 print 'amplitude of vprime (in ms^(-1)), psi0*k, is:', psi0*k #amplitude of vprime
@@ -18,7 +19,7 @@ print 'Scaling factor to dimensionalise velocity equations (in ms^(-1)), L*f0 is
 print 'uprime_nd is: -((psi0*np.pi)/((L**2)*f0))*np.cos(k*L*x - c*T*t)*np.cos(np.pi*y)'
 print 'vprime_nd is: -((psi0*k)/(L*f0))*np.sin(k*L*x - c*T*t)*np.sin(np.pi*y)'
 
-print 'amplitude of uprime_nd, (psi0*pi)/(L**2*f0), is:', A_und
+print 'amplitude of uprime_nd, (psi0*pi)/((L**2)*f0), is:', A_und
 print 'amplitude of vprime_nd, (psi0*k)/(L*f0), is:', A_vnd
 
 print 'k is:', k
@@ -27,196 +28,148 @@ print 'k*L is:', k*L
 
 print 'c is:', c
 print 'T is:', T
-print 'c*T is:', c*T
+print 'k*c*T is:', k*c*T
 
 ###############################################################################################
 
 def main():
-    xvalues = np.linspace(0,3,100)
+    #Define dimensions of arrays
+    xvalues = np.linspace(0,1,100)
     yvalues = np.linspace(0,1,100) #Channel width in y direction is 1*L
-    tvalues = np.linspace(0,1,100)
 
-    #print 'xvalues are:', xvalues
+    time = 0 #Choose time step (in units of T)
 
     xlength = len(xvalues)
     ylength = len(yvalues)
-    tlength = len(tvalues)
+
+###############################################################################################
 
     #CREATE ARRAY FOR PSIPRIME VALUES
-    psiprime_nd = np.zeros((ylength,xlength,tlength)) #Note x and y have switched so plotted output is oriented correctly
+    psiprime_nd = np.zeros((ylength,xlength)) #Note x and y have switched so plotted output is oriented correctly
 
-    #Indices in array go [y,x,t]
     for i in range(0, ylength, 1):
         for j in range(0, xlength, 1):
-            for m in range(0, tlength, 1):
-                #print 'x is: ', xvalues[j]
-                #print 'y is: ', yvalues[i]
-                #print 't is: ', tvalues[k]
-
-                psiprime_nd[i,j,m] = psiprime(x=xvalues[j], y=yvalues[i], t=tvalues[m])
-                #print 'uprime_nd is (from function):', uprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
-                #print 'uprime_nd is (from array):', uprime_nd[i,j,k]
+            psiprime_nd[i,j] = psiprime(x=xvalues[j], y=yvalues[i], t=time)
 
     print 'type of psiprime_nd is:', type(psiprime_nd)
     print 'shape of psiprime_nd is:', psiprime_nd.shape
-    #print 'uprime_nd x values are:', uprime_nd[:,0,0]
 
 ###############################################################################################
 
     #CREATE ARRAY FOR UPRIME VALUES
-    uprime_nd = np.zeros((ylength,xlength,tlength)) #Note x and y have switched so plotted output is oriented correctly
+    uprime_nd = np.zeros((ylength,xlength)) #Note x and y have switched so plotted output is oriented correctly
 
-    #Indices in array go [y,x,t]
     for i in range(0, ylength, 1):
         for j in range(0, xlength, 1):
-            for m in range(0, tlength, 1):
-                #print 'x is: ', xvalues[j]
-                #print 'y is: ', yvalues[i]
-                #print 't is: ', tvalues[k]
-
-                uprime_nd[i,j,m] = uprime(x=xvalues[j], y=yvalues[i], t=tvalues[m])
-                #print 'uprime_nd is (from function):', uprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
-                #print 'uprime_nd is (from array):', uprime_nd[i,j,k]
+            uprime_nd[i,j] = uprime(x=xvalues[j], y=yvalues[i], t=time)
 
     print 'type of uprime_nd is:', type(uprime_nd)
     print 'shape of uprime_nd is:', uprime_nd.shape
-    #print 'uprime_nd x values are:', uprime_nd[:,0,0]
 
 ###############################################################################################
 
     #CREATE ARRAY FOR VPRIME VALUES
-    vprime_nd = np.zeros((ylength,xlength,tlength))
+    vprime_nd = np.zeros((ylength,xlength))
 
-    #Indices in array go [y,x,t]
-    for i in range(0, ylength, 1): #Note x and y have switched so plotted output is oriented correctly
+    for i in range(0, ylength, 1):
         for j in range(0, xlength, 1):
-            for m in range(0, tlength, 1):
-                #print 'x is: ', xvalues[j]
-                #print 'y is: ', yvalues[i]
-                #print 't is: ', tvalues[k]
+            vprime_nd[i,j] = vprime(x=xvalues[j], y=yvalues[i], t=time)
 
-                vprime_nd[i,j,m] = vprime(x=xvalues[j], y=yvalues[i], t=tvalues[m])
-                #print 'vprime_nd is (from function):', vprime(x=xvalues[j], y=yvalues[i], t=tvalues[k])
-                #print 'vprime_nd is (from array):', vprime_nd[i,j,k]
 
     print 'type of vprime_nd is:', type(vprime_nd)
     print 'shape of vprime_nd is:', vprime_nd.shape
-    #print 'vprime_nd x values are:', vprime_nd[:,0,0]
 
-    print 'vprime_nd at y = 0 is:', vprime_nd[0,2,2]
-    print 'vprime_nd at y = L is:', vprime_nd[-1,2,2]
-
-    '''
-    #Try to find streamlines by dividing v over u
-    voveru = np.divide(vprime_nd, uprime_nd)
-    print 'type of voveru is:', type(voveru)
-    print 'shape of voveru is:', voveru.shape
-    '''
+    #Check boundary conditions for v
+    print 'vprime_nd at y = 0 is:', vprime_nd[0,10]
+    print 'vprime_nd at y = L is:', vprime_nd[-1,10]
 
 ###############################################################################################
 
     #DEFINE ODE TO INTEGRATE FOR PARCEL TRAJECTORIES
 
-    #SOMETHING VERY WRONG WITH ORDERS OF MAGNITUDE SOMEWHERE. AMPLITUDES ARE ~1e-3, cT~1e2,
-    #WHICH MEANS 1e-5 SCALING OF TRAJECTORIES. VERY SMALL LENGTHS, VERY FAST OSCILLATIONS.
+    #SOMETHING VERY WRONG WITH ORDERS OF MAGNITUDE SOMEWHERE? AMPLITUDES ARE ~1e-4, cT~1e2,
+    #WHICH MEANS 1e-6 SCALING OF TRAJECTORIES. VERY SMALL LENGTHS, VERY FAST OSCILLATIONS.
     #WHAT PARAMETERS ARE INCORRECT HERE?
 
-    #TRY SOLVING THIS ANALYTICALLY BY HAND AND PLOTTING THAT
+    #MAYBE ODEINT IS THE WRONG INTEGRATOR TO USE?? ARE U AND V ACTUALLY UPDATED WITH EACH TIMESTEP?
 
-    #CURRENTLY SOLVING UPRIME AND VPRIME WITH AMPLITUDES = 1.
-    def velocitynd(s,t):
+    def velocity(s,t):
         x, y = s
         dsdt = [uprime(x=x,y=y,t=t), vprime(x=x,y=y,t=t)]
         #dsdt = [testuprime(x=x,y=y,t=t), testvprime(x=x,y=y,t=t)]
         return dsdt
 
-    t=np.linspace(0,0.3,500)
+    t=np.linspace(0,100,500)
 
-    s0_a = [0, 0.5]
-    s0_b = [0, 0.4999]
+    #s0_a = [0.47, 0.1] #oscillatory trajectory
+    #s0_e = [0.7857, 0.5] #oscillatory trajectory
+    s0_a = [0.47, 0.1]
+    s0_b = [0.5, 0.5]
 
-    print 'trajectory a initial x position:', A_und*L*s0_a[0]
-    print 'trajectory a initial y position:', A_vnd*L*s0_a[1]
+    '''
+    s0_c = [0.47, 0.12]
+    s0_d = [0.47, 0.13]
+    s0_e = [0.47, 0.14]
+    s0_f = [0.47, 0.15]
+    '''
 
-    print 'trajectory b initial x position:', A_und*L*s0_b[0]
-    print 'trajectory b initial y position:', A_vnd*L*s0_b[1]
+    sol_a = odeint(velocity, s0_a, t, mxstep=1000000)
+    sol_b = odeint(velocity, s0_b, t, mxstep=1000000)
+    #sol_c = odeint(velocity, s0_c, t, mxstep=1000000)
+    #sol_d = odeint(velocity, s0_d, t, mxstep=1000000)
+    #sol_e = odeint(velocity, s0_e, t, mxstep=1000000)
+    #sol_f = odeint(velocity, s0_f, t, mxstep=1000000)
 
-    print 'initial separation is:', np.sqrt((A_und*L*s0_a[0]-A_und*L*s0_b[0])**2 + (A_vnd*L*s0_a[1]-A_vnd*L*s0_b[1])**2)
+    print 'type of trajectory solution is:', type(sol_a)
+    print 'shape of trajectory solution is:', sol_a.shape
 
-    sol_a = odeint(velocitynd, s0_a, t, mxstep=1000000)
-    sol_b = odeint(velocitynd, s0_b, t, mxstep=1000000)
 
 ###############################################################################################
 
-    #ATTEMPT TO TRANSFORM TRAJECTORIES TO EDDY REFERENCE FRAME?
-
-    shift_a = np.array(t*(uprime(x=sol_a[:,0], y=sol_a[:,1], t=t), vprime(x=sol_a[:,0], y=sol_a[:,1], t=t)))
+    #ATTEMPT TO TRANSFORM TRAJECTORIES TO WAVE REFERENCE FRAME
+    shift = np.zeros_like(sol_a)
+    shift[:,0] = c*t
 
     print 'shape of sol_a is:', sol_a.shape
-    print 'shape of shift is:', shift_a.shape
-    print 'shape of transposed shift is:', shift_a.transpose().shape
+    print 'shape of shift is:', shift.shape
+    print 'shape of transposed shift is:', shift.transpose().shape
 
-    #N.B. need to transpose to agree with shape of sol_a array
-    rel_sol_a = sol_a - shift_a.transpose() #new parcel trajectory relative to WHAT???
-
-
-    shift_b = np.array(t*(uprime(x=sol_b[:,0], y=sol_b[:,1], t=t), vprime(x=sol_b[:,0], y=sol_b[:,1], t=t)))
-    rel_sol_b = sol_b - shift_b.transpose()
-
-    print 'trajectory a final x position:', A_und*L*rel_sol_a[-1,0]
-    print 'trajectory a final y position:', A_vnd*L*rel_sol_a[-1,1]
-
-    print 'trajectory b final x position:', A_und*L*rel_sol_b[-1,0]
-    print 'trajectory b final y position:', A_vnd*L*rel_sol_b[-1,1]
-
-    print 'final separation is:', np.sqrt((A_und*L*rel_sol_a[-1,0]-A_und*L*rel_sol_b[-1,0])**2 + (A_vnd*L*rel_sol_a[-1,1]-A_vnd*L*rel_sol_b[-1,1])**2)
+    rel_sol_a = sol_a - shift #new parcel trajectory relative to Rossby wave
+    rel_sol_b = sol_b - shift
 
 ###############################################################################################
 
     #DO ALL THE PLOTTING
-
     fig1 = plt.figure()
 
     ax1=fig1.add_subplot(131)
-    ax1.imshow(psiprime_nd[:,:,0], origin='lower')
+    ax1.imshow(psiprime_nd[:,:], origin='lower')
 
     ax2 = fig1.add_subplot(132)
-    ax2.imshow(uprime_nd[:,:,0], origin='lower')
-
-    #print 'uprime_nd[:,:,0] is:', uprime_nd[:, : ,0]
-    #print 'uprime_nd[:,0,:] is:', uprime_nd[:, 0, :]
-    #print 'uprime_nd[0,:,:] is:', uprime_nd[0, :, :]
+    ax2.imshow(uprime_nd[:,:], origin='lower')
 
     ax3 = fig1.add_subplot(133)
-    ax3.imshow(vprime_nd[:,:,0], origin='lower')
+    ax3.imshow(vprime_nd[:,:], origin='lower')
 
-    #print 'vprime_nd[:,:,0] is:', vprime_nd[:, : ,0]
-    #print 'vprime_nd[:,0,:] is:', vprime_nd[:, 0, :]
-    #print 'vprime_nd[0,:,:] is:', vprime_nd[0, :, :]
 
     fig2 = plt.figure()
     ax4 = fig2.add_subplot(111)
 
-    #MULTIPLYING SOLUTIONS BY THE VELOCITY AMPLITUDES AND BY L. MAKE SURE THIS IS CORRECT.
-    ax4.plot(A_und*L*sol_a[:,0], A_vnd*L*sol_a[:,1], label='trajectory a')
-    ax4.plot(A_und*L*sol_b[:,0], A_vnd*L*sol_b[:,1], label='trajectory b')
+    #Multiplying solutions by L to get physical lengths
+    ax4.plot(L*sol_a[:,0], L*sol_a[:,1], label='a')
+    ax4.plot(L*sol_b[:,0], L*sol_b[:,1], label='b')
+    #ax4.plot(L*sol_c[:,0], L*sol_c[:,1], label='c')
+    #ax4.plot(L*sol_d[:,0], L*sol_d[:,1], label='d')
+    #ax4.plot(L*sol_e[:,0], L*sol_e[:,1], label='e')
+    #ax4.plot(L*sol_f[:,0], L*sol_f[:,1], label='f')
 
-    ax4.plot(A_und*L*rel_sol_a[:,0], A_vnd*L*rel_sol_a[:,1], color='black', label ='trajectory a in different frame')
-    ax4.plot(A_und*L*rel_sol_b[:,0], A_vnd*L*rel_sol_b[:,1], color='red', label ='trajectory b in different frame')
+    ax4.plot(L*rel_sol_a[:,0], L*rel_sol_a[:,1], color='black', label ='a in RW frame')
+    ax4.plot(L*rel_sol_b[:,0], L*rel_sol_b[:,1], color='red', label ='b in RW frame')
 
-    #ax4.imshow(psiprime_nd[:,:,50], origin='lower', extent=[-3*L,0,0,L])
+    #Overlay initial streamfunction to see if trajectories make sense.
+    #ax4.imshow(psiprime_nd[:,:], origin='lower', extent=[0,L,0,L])
 
     plt.legend(loc='best')
-
-    '''
-    #Plot vprime over uprime
-    fig3 = plt.figure()
-    ax5 = fig3.add_subplot(111)
-    ax5.set_xlabel('x')
-    ax5.set_ylabel('y')
-    ax5.set_title('v over u')
-    voverucontour = plt.contour(voveru[:,:,0], origin = 'lower')
-    '''
 
     plt.show()
 
